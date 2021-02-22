8dc99964b1ab3654a11ff30b3b881936dbbb99c6
Raúl <raul.pineda.caballero@gmail.com>
2021-02-22 20:26:25 +0100

    ya esta el metodo retirar funcionando

--- Ejercicio1-Banco/src/Main.java
+++ Ejercicio1-Banco/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 		System.out.println("Introduce el número de clientes");
 		n = sc.nextInt();
 		
-		Cuenta listCuenta [] = new Cuenta[n]; // Creamos los objetos cuentas en un array
+		Cuenta listCuenta [] = new Cuenta[n+1]; // Creamos los objetos cuentas en un array
 
 	
 		añadirCliente(listCuenta); // añadimos el nombre y el sueldo
@@ -24,9 +24,9 @@ public class Main {
 	}
 	
 	public static void añadirCliente(Cuenta listCuenta[]) {
-		for (int i = 0; i < listCuenta.length; i++) {
+		for (int i = 1; i < listCuenta.length; i++) {
 			System.out.println("--------------------");
-			System.out.println("-Añadir cliente " + (i+1)+ "º-");
+			System.out.println("-Añadir cliente " + (i)+ "º-");
 			System.out.println("--------------------");
 			System.out.println("Para darse de alta debe de indicar su nombre_");
 		    titular = sc.next();
@@ -37,7 +37,7 @@ public class Main {
 			System.out.println("------------------------------------");
 			op = sc.nextInt();
 			
-			switch (op) { // Elige en
+			switch (op) {
 				case 1: {
 					System.out.println("Escribe la cantidad deseada");
 					saldo = sc.nextInt();
@@ -49,40 +49,43 @@ public class Main {
 					break;
 				}
 			}
-			
-			
-			
-			
-			
-			
+		
 	}
+		
+	
 	}
 	
 
 		public static void retirarDinero(Cuenta listCuenta[]) {
 			
-			for (int i = 0; i < listCuenta.length; i++) {
-			System.out.println("Introduce Cliente para quien retirar dinero:_");
-			titular = sc.next();
-			System.out.println("------------------------------------");
-			System.out.println("-Desea retirar dinero en su cuenta-");
-			System.out.println("-1.Sí                              -");
-			System.out.println("-2.No                              -");
-			System.out.println("------------------------------------");
-			op = sc.nextInt();
+			for (int i = 1; i < listCuenta.length; i++) {
+
+				System.out.println("------------------------------------");
+				System.out.println("-Desea retirar dinero de alguna cuenta-");
+				System.out.println("-1.Sí                              -");
+				System.out.println("-2.No                              -");
+				System.out.println("------------------------------------");
+				op = sc.nextInt();
+				
+				
 			
 			if(op == 1) {
-				System.out.println(listCuenta[i].getSaldo() + " " + listCuenta[i].getNombre());
 				
-					System.out.println("Escribe la cantidad deseada");
-					saldoretirar = sc.nextInt();
+				System.out.println("Lista de clientes registrados la sucursal bancaria");
+				for (int i1 = 1; i1 < listCuenta.length; i1++) { // Imprimimos los nombres de los usuarios registrados con su correspondiente saldo
+					System.out.println((i1)+"º cliente " + "[" + listCuenta[i1].getNombre()+"]" + "|| Saldo disponible: " + listCuenta[i1].getSaldo());
+				}
 			
-					Cuenta.setSaldo = listCuenta[i].getSaldo() - saldoretirar;
-					System.out.println(listCuenta[i].getSaldo() + " " + listCuenta[i].getNombre());
+				System.out.println("Marque la cuenta deseada");
+				int nCuenta = sc.nextInt();
+				System.out.println("Escribe la cantidad deseada, a retirar");
+					saldoretirar = sc.nextInt();
+					int dRetirado = (int) (listCuenta[nCuenta].getSaldo() - saldoretirar); // almacenamos la diferencia en una variable
+					listCuenta[nCuenta].setSaldo(dRetirado); // pasamos la variable con el dato que queremos cambiar
+					System.out.println("Su saldo ahora es: " + listCuenta[nCuenta].getSaldo() + "|| De la cuenta " + listCuenta[nCuenta].getNombre()); // imprimimos la cuenta actualizada con el nombre del cliente
 					
-			} else if(listCuenta[i].getSaldo()<0) {
-				Cuenta.setSaldo = 0;
-				System.out.println(listCuenta[i].getSaldo() + " " + listCuenta[i].getNombre());
+			} else if(listCuenta[i].getSaldo()<0) { // si marca 2 por defecto acabara el prorama
+				System.out.println("Sus operaciones han finalizado, vuelva cuando quiera");
 			}
 			
 			
