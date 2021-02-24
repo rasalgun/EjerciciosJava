a4691b44e847355246180a92aa11f6a8169752e9
Raúl <raul.pineda.caballero@gmail.com>
2021-02-24 17:58:51 +0100

    actualizado

---
+++ Ejercicio3/src/Persona.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class Persona {
+
+	String nombre, dni, profesion;
+	int edad, peso, altura;
+	char sexo;
+	double salario;
+	
+	public Persona(String nombre, int edad, String dni, char sexo, String profesion, double salario, int peso, int altura) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.profesion = profesion;
+		this.salario = salario;
+		this.peso = peso;
+		this.altura = altura;
+	}
+}
+
