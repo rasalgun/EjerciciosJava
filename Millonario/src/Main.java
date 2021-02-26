a6f9ca17a2d5eea6fde922cee7b56610bd6171f7
Raúl <raul.pineda.caballero@gmail.com>
2021-02-26 08:58:52 +0100

    getters y setters

--- Millonario/src/Main.java
+++ Millonario/src/Main.java
@@ -1,4 +1,10 @@
+import java.util.*;
 
 public class Main {
-
+	public static void main(String[] args){
+		System.out.println("------------------------------------------------------");
+		System.out.println("-HAS ENTRADO AL CONCURSO DE QUIERO SER MILLONARIO     -");
+		System.out.println("-DEBERAS DE RESPONDER 15 PREGUNTAS Y NO FALLAR NINGUNA-");
+		System.out.println("------------------------------------------------------");
+	}
 }
--- Millonario/src/Pregunta.java
+++ Millonario/src/Pregunta.java
@@ -1,4 +1,102 @@
 
 public class Pregunta {
+	String enunciado;
+	String opcion1;
+	String opcion2;
+	String opcion3;
+	String opcion4;
+	int l1;
+	int l2;
+	int l3;
+	int l4;
+	
+	public Pregunta(String enunciado, String opcion1, String opcion2, String opcion3, String opcion4, int l1, int l2, int l3, int l4) {
+		super();
+		this.enunciado = enunciado;
+		this.opcion1 = opcion1;
+		this.opcion2 = opcion2;
+		this.opcion3 = opcion3;
+		this.opcion4 = opcion4;
+		this.l1 = l1;
+		this.l2 = l2;
+		this.l3 = l3;
+		this.l4 = l4;
+	}
 
+	public String getEnunciado() {
+		return enunciado;
+	}
+
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+
+	public String getOpcion1() {
+		return opcion1;
+	}
+
+	public void setOpcion1(String opcion1) {
+		this.opcion1 = opcion1;
+	}
+
+	public String getOpcion2() {
+		return opcion2;
+	}
+
+	public void setOpcion2(String opcion2) {
+		this.opcion2 = opcion2;
+	}
+
+	public String getOpcion3() {
+		return opcion3;
+	}
+
+	public void setOpcion3(String opcion3) {
+		this.opcion3 = opcion3;
+	}
+
+	public String getOpcion4() {
+		return opcion4;
+	}
+
+	public void setOpcion4(String opcion4) {
+		this.opcion4 = opcion4;
+	}
+
+	public int getL1() {
+		return l1;
+	}
+
+	public void setL1(int l1) {
+		this.l1 = l1;
+	}
+
+	public int getL2() {
+		return l2;
+	}
+
+	public void setL2(int l2) {
+		this.l2 = l2;
+	}
+
+	public int getL3() {
+		return l3;
+	}
+
+	public void setL3(int l3) {
+		this.l3 = l3;
+	}
+
+	public int getL4() {
+		return l4;
+	}
+
+	public void setL4(int l4) {
+		this.l4 = l4;
+	}
+	
+	
+	// Funciones y metodos de la clase pregunta
+	
+	
 }
