3b3fea5e1554f9c053ebae1992cd92820ec819ac
Raúl <raul.pineda.caballero@gmail.com>
2021-02-26 09:04:34 +0100

    metodo preguntas

--- Millonario/src/Main.java
+++ Millonario/src/Main.java
@@ -2,9 +2,21 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args){
+		
+		
+		
+		Pregunta listPregunta[] = new Pregunta[15];
+		
+			
 		System.out.println("------------------------------------------------------");
 		System.out.println("-HAS ENTRADO AL CONCURSO DE QUIERO SER MILLONARIO     -");
 		System.out.println("-DEBERAS DE RESPONDER 15 PREGUNTAS Y NO FALLAR NINGUNA-");
 		System.out.println("------------------------------------------------------");
+		
+		
+	}
+	
+	public static void preguntas() {
+		
 	}
 }
